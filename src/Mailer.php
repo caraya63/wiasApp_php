7b1b150c92f0bd65d4939912c3541ab5662c260e
caraya63 <45213769+caraya63@users.noreply.github.com>
2026-02-13 18:43:21 -0300

    actualizacion

---
+++ src/Mailer.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+final class Mailer
+{
+    public static function send(string $to, string $subject, string $html, string $text = ''): bool
+    {
+        $mail = new PHPMailer(true);
+
+        try {
+            // SMTP
+            $mail->isSMTP();
+            $mail->Host = Config::SMTP_HOST;
+            $mail->SMTPAuth = true;
+            $mail->Username = Config::SMTP_USER;
+            $mail->Password = Config::SMTP_PASS;
+            $mail->SMTPSecure = Config::SMTP_SECURE; // 'tls' o 'ssl'
+            $mail->Port = Config::SMTP_PORT;
+
+            $mail->setFrom(Config::MAIL_FROM, Config::MAIL_FROM_NAME);
+            $mail->addAddress($to);
+
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->Body = $html;
+            $mail->AltBody = $text !== '' ? $text : strip_tags($html);
+
+            return $mail->send();
+        } catch (Exception $e) {
+            error_log("MAIL ERROR: " . $e->getMessage());
+            return false;
+        }
+    }
+}
